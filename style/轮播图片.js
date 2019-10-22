293e8211d6c7e5e0576ddde30f2d9bb92c0c6798
akasiop <1209977477@qq.com>
2019-10-22 19:05:57 +0800

    03

---
+++ style/轮播图片.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    movies: [
+      { url: '/images/f01.jpg' },
+      { url: '/images/f00.png' },
+      { url: '/images/f01.jpg' },
+      { url: '/images/f00.png' }
+    ]
+  }
+})
